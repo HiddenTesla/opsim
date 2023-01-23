a885db127b840852d00b58582633e54714a74088
ChenYanqin <yanqinchen@ea.com>
2023-01-23 17:40:41 +0800

    Render artifact

--- frontend/src/Component/Create.tsx
+++ frontend/src/Component/Create.tsx
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
 import {Input, Button} from "antd";
 import {Request} from "../Request";
+import {Artifact} from "../Model/Artifact";
+import {ArtifactRenderer} from "../Model/ArtifactRenderer";
 
-type Props = {}
-type States = {}
+type Props = {
+}
+type States = {
+  artifact: Artifact,
+}
 
 class Create extends Component<Props, States> {
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      artifact: new Artifact(),
+    }
+  }
 
 
   render() {
@@ -15,6 +27,7 @@ class Create extends Component<Props, States> {
         <Button onClick={this.onButtonClick.bind(this)}>
           建一个新的遗物
         </Button>
+      <div>{ArtifactRenderer.render(this.state.artifact)}</div>
       </div>
     )
   }
@@ -23,7 +36,9 @@ class Create extends Component<Props, States> {
   async onButtonClick() {
     let resp = await Request.createArtifact()
     console.log(resp);
-
+    this.setState({
+      artifact: resp,
+    })
   }
 
 }
--- frontend/src/Model/Artifact.tsx
+++ frontend/src/Model/Artifact.tsx
@@ -1,17 +1,65 @@
 
-class Artifact {
-  artifactId: number;
+enum ArtifactType {
+  UNDEFINED = "UNDEFINED",
+  FLOWER    = "FLOWER",
+  PLUME     = "PLUME",
+  SANDS     = "SANDS",
+  GOBLET    = "GOBLET",
+  CIRCLET   = "CIRCLET",
+}
 
+enum StatType {
+  UNDEFINED             = "UNDEFINED",
+  HP_PERCENTAGE         = "HP_PERCENTAGE",
+  HP_FIXED              = "HP_FIXED",
+  ATK_PERCENTAGE        = "ATK_PERCENTAGE",
+  ATK_FIXED             = "ATK_FIXED",
+  DEF_PERCENTAGE        = "DEF_PERCENTAGE",
+  DEF_FIXED             = "DEF_FIXED",
+  ELEMENTAL_MASTERY     = "ELEMENTAL_MASTERY",
+  ENERGY_RECHARGE       = "ENERGY_RECHARGE",
+  PHYSICAL_DAMAGE_BONUS = "PHYSICAL_DAMAGE_BONUS",
+  ANEMO_DAMAGE_BONUS    = "ANEMO_DAMAGE_BONUS",
+  GEO_DAMAGE_BONUS      = "GEO_DAMAGE_BONUS",
+  ELECTRO_DAMAGE_BONUS  = "ELECTRO_DAMAGE_BONUS",
+  DENDRO_DAMAGE_BONUS   = "DENDRO_DAMAGE_BONUS",
+  HYDRO_DAMAGE_BONUS    = "HYDRO_DAMAGE_BONUS",
+  PYRO_DAMAGE_BONUS     = "PYRO_DAMAGE_BONUS",
+  CRYO_DAMAGE_BONUS     = "CRYO_DAMAGE_BONUS",
+  CRIT_RATE             = "CRIT_RATE",
+  CRIT_DAMAGE           = "CRIT_DAMAGE",
+  HEALING_BONUS         = "HEALING_BONUS",
+}
+
+class Stat {
+  type:  StatType;
+  value: number;
   constructor() {
-    this.artifactId = 0;
-  }
-
-  static fromJson(obj: any): Artifact {
-    let artifact = new Artifact();
-    artifact.artifactId = obj.artifactId;
-
-    return artifact;
+    this.type = StatType.UNDEFINED;
+    this.value = 0;
   }
 }
 
-export {Artifact};
+class Artifact {
+  artifactId: number;
+  type:       ArtifactType;
+  level:      number;
+  mainStat:   Stat;
+  subStats:   Stat[];
+
+  constructor() {
+    this.artifactId = 0;
+    this.type = ArtifactType.UNDEFINED;
+    this.level = 0;
+    this.mainStat = new Stat();
+    this.subStats = [];
+  }
+
+}
+
+export {
+  ArtifactType,
+  Artifact,
+  StatType,
+  Stat,
+};
